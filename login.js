aafe8f7c5f1b691be904950a7de3c7378ff4b598
guitador123 <153464845+guitador123@users.noreply.github.com>
2023-12-14 23:26:32 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -4,9 +4,7 @@ function auth(){
     var pass =  String(document.getElementById('passtxt').value)
     if(login == 'guiberchol123@outlook.com' && pass == '10121330'){
         window.alert('Sucessfully Login')
-        setTimeout(function(){
             window.location.replace("https://guitador123.github.io/paperluxeapp/modelo")
-        }, 2000)
     } else{
         window.alert('Wrong Login')
     }
