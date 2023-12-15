99b8f651e49c54827eedbfc53ccc5ad554c6386a
guitador123 <153464845+guitador123@users.noreply.github.com>
2023-12-14 23:34:10 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,1 @@
 
-function auth(){
-    var login = String(document.getElementById('logintxt').value)
-    var pass =  String(document.getElementById('passtxt').value)
-    if(login == 'guiberchol123@outlook.com' && pass == '10121330'){
-        window.alert('Sucessfully Login')
-            window.location.replace("https://guitador123.github.io/paperluxeapp/modelo")
-    } else{
-        window.alert('Wrong Login')
-    }
-}
